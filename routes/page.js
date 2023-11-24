4078e16139d70aa482b14307a79bd8e9668daa19
yeeo01 <pyje000@naver.com>
2023-11-24 10:56:29 +0900

    Remove node_modules

--- routes/page.js
+++ routes/page.js
@@ -13,7 +13,9 @@ router.use((req, res, next) => {
   res.locals.user = req.user;
   res.locals.followerCount = req.user?.Followers?.length || 0;
   res.locals.followingCount = req.user?.Followings?.length || 0;
-  res.locals.followingIdList = req.user?.Followings?.map((f) => f.id) || [];
+  res.locals.followingIdList = (req.user?.Followings || []).map((f) => f.id);
+  //Followings 배열을 가져와서 undefined 또는 null인 경우 빈 배열로, 유효하다면 id 속성을 추출하여 새로운 배열을 생성
+
   next();
 });
 
